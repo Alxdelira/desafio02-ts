a7e62a3523b1fb5b9da71cb3eb8fa42c0fe0250b
Alexandre <alx.delira@gmail.com>
2024-03-05 17:40:17 -0400

    Fazendo tela de login

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,13 @@
 import { Box, Center, Text, Flex, Square, Heading, Spacer, ButtonGroup, Button } from "@chakra-ui/react"
+import { SingUp } from "./SingUp"
 
-export const Footer = () => {
+export const Header = () => {
   return (
-    <Flex minWidth='max-content' alignItems='center' gap='2' height='250px'>
+    <Flex minWidth='max-content' alignItems='center' gap='2' p={15}>
       <Box p='2'>
         <Heading size='md'>DIO_BANKING</Heading>
-      </Box>     
+      </Box>
+      <Spacer />      
     </Flex>
   )
 }
-
---
+++ src/components/SingUp.tsx
@@ -0,0 +1,13 @@
+import { Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react"
+import { Login } from "./Login"
+
+
+export const SingUp = ({ children }: any) => {
+
+
+    return (
+        <>
+         
+        </>
+    )
+}
